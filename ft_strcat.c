df63dfdd9267fa119b94faa554880b71fe84cbb3
Losviz99 <43620890+Losviz99@users.noreply.github.com>
2026-03-27 17:35:02 +0100

    Update ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -9,6 +9,9 @@
 /*   Updated: 2026/03/27 12:53:43 by lsvizzer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
 char	*ft_strcat(char *dest, char *src)
 {
 	char *res;
@@ -25,4 +28,18 @@ char	*ft_strcat(char *dest, char *src)
 	*dest = '\0';
 	return (res);
 }
+
+int main()
+{
+	char dest1[50] = "Frodo ";
+	char dest2[50] = "Baggins ";
+	char dest3[50] = "";
+	char dest4[50] = "Test";
 	
+	printf("Test 1: %s\n", ft_strcat(dest1, "Baggins"));
+	printf("Test 2: %s\n", ft_strcat(dest2, "Frodo"));
+	printf("Test 3: %s\n", ft_strcat(dest3, "Empty"));
+	printf("Test 4: %s\n", ft_strcat(dest4, ""));
+	
+	return (0);
+}
