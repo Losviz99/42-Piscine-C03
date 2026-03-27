6217d2a8f927c80411b7ccf6ee7a6bcdc4ae7231
Losviz99 <43620890+Losviz99@users.noreply.github.com>
2026-03-27 17:58:16 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 unsigned int	ft_strlen(char *str)
 {
 	int	len;
@@ -41,4 +43,17 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	}
 	dest[dest_len + i] = '\0';
 	return (dest_len + src_len);
+}
+
+int main()
+{
+	char dest1[20] = "Frodo ";
+	char dest2[20] = "Gandalf ";
+	char dest3[10] = "";
+	
+	printf("Test 1: %u - %s\n", ft_strlcat(dest1, "Baggins", 20), dest1);
+	printf("Test 2: %u - %s\n", ft_strlcat(dest2, "Il grigio", 20), dest2);
+	printf("Test 3: %u - %s\n", ft_strlcat(dest3, "Vouta", 10), dest3);
+	
+	return (0);
 }
