f174db66b9cc8032582b1d3eb363b8ed42c91278
Losviz99 <43620890+Losviz99@users.noreply.github.com>
2026-03-27 12:59:38 +0100

    Add files via upload

---
+++ ft_strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsvizzer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 12:56:25 by lsvizzer          #+#    #+#             */
+/*   Updated: 2026/03/27 12:56:47 by lsvizzer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] != '\0' && (dest_len + i) < (size))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
---
+++ ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsvizzer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 12:54:56 by lsvizzer          #+#    #+#             */
+/*   Updated: 2026/03/27 12:55:35 by lsvizzer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	char	*match_start;
+	int		check_pos;
+
+	match_start = 0;
+	while (*str != '\0')
+	{
+		check_pos = 0;
+		match_start = str;
+		while (to_find[check_pos] != '\0')
+		{
+			if (to_find[check_pos] != *str)
+			{
+				match_start = 0;
+				break ;
+			}
+			check_pos++;
+			str++;
+		}
+		if (to_find[check_pos] == '\0')
+			break ;
+		str++;
+	}
+	return (match_start);
+}
